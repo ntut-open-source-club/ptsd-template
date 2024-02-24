d8f816142e2e67f54c8c3e9be296611cb901ce84
Rick <t110590016@ntut.org.tw>
2024-02-24 15:37:39 +0800

    Remove the GA

--- files.cmake
+++ files.cmake
@@ -1,21 +1,9 @@
 set(SRC_FILES
-    AnimatedCharacter.cpp
-    AppEnd.cpp
-    AppStart.cpp
-    AppUpdate.cpp
-    AppUtil.cpp
-    Character.cpp
-    PhaseManager.cpp
+    App.cpp
 )
 
 set(INCLUDE_FILES
-    AnimatedCharacter.hpp
     App.hpp
-    AppUtil.hpp
-    BackgroundImage.hpp
-    Character.hpp
-    PhaseResourceManger.hpp
-    TaskText.hpp
 )
 
 set(TEST_FILES
