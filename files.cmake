8706fbfc2b313666fdb3085cabfddda21d46abe2
Rick <t110590016@ntut.org.tw>
2024-02-25 13:31:19 +0800

    Add files.cmake

---
+++ files.cmake
@@ -0,0 +1,22 @@
+set(SRC_FILES
+    AnimatedCharacter.cpp
+    AppEnd.cpp
+    AppStart.cpp
+    AppUpdate.cpp
+    AppUtil.cpp
+    Character.cpp
+    PhaseManager.cpp
+)
+
+set(INCLUDE_FILES
+    AnimatedCharacter.hpp
+    App.hpp
+    AppUtil.hpp
+    BackgroundImage.hpp
+    Character.hpp
+    PhaseResourceManger.hpp
+    TaskText.hpp
+)
+
+set(TEST_FILES
+)
